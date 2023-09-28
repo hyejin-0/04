eb19047fb4b3b4f866f6ba4ae75bb7a703103476
hyejin-0 <hj111800@naver.com>
2023-09-28 21:11:42 +0900

    4

--- main.c
+++ main.c
@@ -6,11 +6,22 @@
 int main(int argc, char *argv[]) 
 {
 	int a;
-
-	printf("input seconds : ");
+	
+	printf("input a year : ");
 	scanf("%i", &a);
 	
-	printf("%i seconds - %i:%i \n", a, a/60, a%60);
+	//1. 4로 나누어 떨어져야 함 
+	//2. 100으로 나누어 떨어지지 않아야 함
+	//3. 위를 만족 안 해도 400으로 나누어 떨어짐 
+	
+	if ( ( a%4==0 && a%100!=0 ) || (a%400==0) )
+	{
+		printf("%i is leap year.\n", a);
+	} 
+	else
+	{
+		printf("%i is not leap year.\n", a);
+	}
 	
 	return 0;
 }
